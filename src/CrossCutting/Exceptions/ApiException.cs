ef33cedc857c398bc6ca9210556ceb8f422a7892
mccesar <mccesar@stefanini.com>
2022-01-27 18:25:45 -0300

    New exception to treat APIs returns.

---
+++ src/CrossCutting/Exceptions/ApiException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossCutting.Exceptions
+{
+    public class ApiException : Exception
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public string Reason { get; set; }
+        public string ResponseBody { get; set; }
+    }
+}
